0d9e85638c71d677580f1aed469f7d92719abe19
Hassler <alexandre@hassler.fr>
2019-02-13 20:33:30 +0100

    port 4000

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+app.listen(PORT, function() {
+  console.log(`listening on port: ${PORT}`)
 })
