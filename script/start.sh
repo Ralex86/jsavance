4b526847a9416192645e6401920a642fc499c438
Hassler <alexandre@hassler.fr>
2019-03-04 17:57:09 +0100

    relative path database

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,5 @@
 yarn install;
 node_modules/.bin/webpack --progress --hide-modules;
 docker run -p 3306:3306 --name docker-mysql -e MYSQL_ROOT_PASSWORD=PASSWORD -e MYSQL_DATABASE=moviedb -d mysql:5.7;
-docker exec -i docker-mysql mysql -uroot -pPASSWORD moviedb < ./database/migration.sql;
+docker exec -i docker-mysql mysql -uroot -pPASSWORD moviedb < database/migration.sql;
 node ./index.js;
