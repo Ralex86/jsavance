625c4aa90e61884c257bd1cbeff30ebb5af6194a
Hassler <alexandre@hassler.fr>
2019-03-04 18:10:54 +0100

    password

--- script/start.sh
+++ script/start.sh
@@ -3,5 +3,5 @@ node_modules/.bin/webpack --progress --hide-modules;
 docker run -p 3306:3306 --name docker-mysql -e MYSQL_ROOT_PASSWORD=PASSWORD -e MYSQL_DATABASE=moviedb -d mysql:5.7;
 echo "waiting for the container to be ready...";
 sleep 15s;
-docker exec -i docker-mysql mysql --user=root --password=PASSWORD moviedb < database/migration.sql;
+docker exec -i docker-mysql mysql --user=root moviedb < database/migration.sql;
 node ./index.js;
